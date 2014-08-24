0de3e569564001cce2364eb36d7c39ee970479dc
sanjeev-chauhan <sanjeevchauhan2585@gmail.com>
2014-08-24 23:37:11 +0530

    Done change for carousel.

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -2,12 +2,13 @@
 
 /* Controllers */
 
-var myStoreControllers = angular.module('myStoreControllers', ['ui.bootstrap']);
+var myStoreControllers = angular.module('myStoreControllers', ['ui.bootstrap', 'ngAnimate']);
 
-myStoreControllers.controller('homePageCtrl', ['$scope', '$http',
-  function($scope, $http) {
+myStoreControllers.controller('homePageCtrl', ['$scope', '$http','$animate',
+  function($scope, $http, $animate) {
 	$scope.latestCollection = [];
-	 
+	$scope.carouselInterval = 3000;
+	$animate.enabled(false,angular.element(document.getElementsByClassName("latestCollectionCarousel")));//Disabling angular animation on carousel as it causes isses with sliding
 	$http({method: 'GET', url: 'static/json/latest.json'}).
 	success(function(data, status, headers, config) {
 	  $scope.latestCollection = data;
