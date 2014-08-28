16b7e534f17fe3969fd3263d8aa70c766f76c5fe
Sanjeev <schauhan2@sapient.com>
2014-08-28 17:18:45 +0530

    Done changes for Products Filter

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -18,20 +18,21 @@ myStoreControllers.controller('homePageCtrl', ['$scope', '$http','$animate','$ro
 	$animate.enabled(false,angular.element(document.getElementsByClassName("latestCollectionCarousel")));//Disabling angular animation on carousel as it causes isses with sliding
 	$http({method: 'GET', url: 'static/json/latest.json'}).
 	success(function(data, status, headers, config) {
-	  $scope.latestCollection = data;
+		$scope.latestCollection = data;
 	})
-		
 }]);
 
 myStoreControllers.controller('productsListCtrl', ['$scope', '$http','$rootScope',
   function($scope, $http, $rootScope) {
-  $rootScope.showBanner = false;//Hide the banner image
-  $http({method: 'GET', url: 'static/json/products-list.json'}).
+	$rootScope.showBanner = false;//Hide the banner image
+	//Fetch products list json to populate results
+	$http({method: 'GET', url: 'static/json/products-list.json'}).
 	success(function(data, status, headers, config) {
-	  $scope.productsList = data;
+		$scope.productsList = data;
 	})
-  
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
+	$scope.filterCategories = ["Shirts" , "Tshirts" ,"Trousers" ,"Jeans" , "Shoes"];
+	$scope.filterBrand = ["Even" ,"Kook N Keech Marvel" ,"Breakbounce" , "White Kalia"];
+	$scope.setImage = function(imageUrl) {
+		$scope.mainImageUrl = imageUrl;
     }
 }]);
