690b949d996c5d1b9455e2018b94aab5b2a36957
sanjeev-chauhan <sanjeevchauhan2585@gmail.com>
2014-08-31 00:28:43 +0530

    Done changes for Filtering , added new product items.

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -34,15 +34,15 @@ myStoreControllersModule.controller('productsListCtrl', ['$scope', '$http','$roo
 	$scope.brand = {};
 	
 	//Function to set checkbox model property true or false in its parent obj when it is checked/unchecked
-	$scope.setFilterBindingProp = function(itemObject){
+	$scope.setFilterBindingProp = function(event,itemObject){
 		$scope[itemObject.dataBindingKey][itemObject.value] = event.target.checked;
 	}
 	
 	$scope.filterCategories = [{dataBindingKey:"category", value:"Shirts"} , {dataBindingKey:"category", value:"Tshirts"} , 
 		{dataBindingKey:"category", value:"Trousers"} ,{dataBindingKey:"category", value:"Jeans"} , {dataBindingKey:"category", value:"Shoes"}];
 		
-	$scope.filterBrand = [{dataBindingKey:"brand", value:"Even"} , {dataBindingKey:"brand", value:"Kook N Keech Marvel"} , 
-		{dataBindingKey:"brand", value:"Breakbounce"}, {dataBindingKey:"brand", value:"White Kalia"}];
+	$scope.filterBrand = [{dataBindingKey:"brand", value:"Even"} , {dataBindingKey:"brand", value:"Puma"} , 
+		{dataBindingKey:"brand", value:"Breakbounce"}, {dataBindingKey:"brand", value:"Indian Terrain"}];
 	
 	$scope.setImage = function(imageUrl) {
 		$scope.mainImageUrl = imageUrl;
