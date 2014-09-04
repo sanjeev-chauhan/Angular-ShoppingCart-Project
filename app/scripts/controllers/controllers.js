afc0640ad5f6f6841fb80e559dff7d95e9c94d35
sanjeev-chauhan <sanjeevchauhan2585@gmail.com>
2014-09-05 00:25:47 +0530

    Done change for product detail page.

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -50,28 +50,30 @@ myStoreControllersModule.controller('productsListCtrl', ['$scope', '$http','$roo
 //Product Detail page controller
 myStoreControllersModule.controller('productDetailCtrl', ['$scope','$http','$routeParams','$rootScope','Products',
 	function($scope,$http,$routeParams,$rootScope,Products) {
+	$scope.activeSize = "";
 		if($scope.activeProduct){
-		$scope.activeProduct =  $rootScope.activeProduct;//Store active product model object from Root Scope
-		$scope.activeThumbnail = $scope.activeProduct.imageUrl;
+			$scope.activeProduct =  $rootScope.activeProduct;//Store active product model object from Root Scope
+			$scope.activeThumbnail = $scope.activeProduct.imageUrl;
 		}
-		else{
+		else{//If active product not set then fetch products list from Products Service and 
 			Products.getProductsList().success(function(data, status, headers, config) {
 				for(var i in data){
 					if($routeParams.productID === data[i].productId){
-						$scope.activeProduct = data[i];
+						$scope.activeProduct = data[i];//Set active product after matching productId from URL
 						$scope.activeThumbnail = $scope.activeProduct.imageUrl;
 					}
 				}
 			});
 		}
-		
-		//If active product not set then fetch products list from Products Service and set active product after matching productId from URL
-		/*if(!$scope.activeProduct){
-		
-		}*/
-		
 		$scope.setActiveImage = function(imageUrl){
 			$scope.activeThumbnail = imageUrl;
+		}
+		$scope.setActiveSize = function(size){
+			$scope.activeSize = size;
+		}
+		
+		$scope.setActiveColor = function(color){
+			$scope.activeColor = color;
 		}
 	}
 ]);
